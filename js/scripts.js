628077c221f7d18be29333f612439c4ff820470d
amitmentos <amit.ronen222@gmail.com>
2023-04-25 21:21:28 +0300

    Tuesday

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,28 @@ $(document).ready(function() {
     var flag2=1;
     var clickedLetters=[];
     var clickedRecs=[];
-    var charArr = ['a','b','a','d','d','b','e','e','f','g','f','g']
-    
+    var charArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    $('#topR-rec').click(function(){
+    $('#topR-rec').click(function(){       
         for (let index = 0; index < 3; index++) {
                 var recNum=$("#game-recs section").length; 
-                if(recNum<12){
-                    var newRec = '<section id="game-rec' + recNum + '">' + charArr[recNum] + '</section>';
+                if(recNum<100){
+                    var newRec = '<section id="game-rec' + recNum + '"></section>';
                     $("#game-recs").append(newRec);   
 
                     var newStyle = '#game-recs #game-rec' + recNum + ' { ' +
                     'width: ' + (startRecSize + 20 * recNum) + 'px; ' +
                     'height: ' + (startRecSize + 20 * recNum) + 'px; ' +
                     '}';
-                    
+                    myCss.insertRule(newStyle, myCss.cssRules.length);
+
+                    var lastSection = $('#game-recs section:last');
+                    var lastSectionBottom = lastSection.position().top + lastSection.outerHeight();
+
                     if(recNum>7 && $(window).width() > 849){
-                        $(".bottom-yellow-rec").css('top', parseFloat($(".bottom-yellow-rec").css('top'))+300);
-                        $(".right-blue-rec").css('height', parseFloat($(".right-blue-rec").css('height'))+300);
-                        $(".left-grey-rec").css('height', parseFloat($(".left-grey-rec").css('height'))+300);
+                        $(".bottom-yellow-rec").css('top', lastSectionBottom+300);
+                        $("#main3").css('height',lastSectionBottom+300);
+                        console.log(lastSection.outerHeight());
                     }
 
                     if(recNum>6&& $(window).width()<=849){
@@ -33,41 +36,71 @@ $(document).ready(function() {
                     }
 
                     if(recNum>0){
-                        
                         var prevTop = $('#game-rec' + (recNum - 1)).position().top;
                         var newTop = $('#game-rec' + recNum).position().top;
                         if (newTop > prevTop || (recNum>9 && recNum-1%3==0)) {
                             $('#game-rec' + recNum).css('margin-left',0);
                         }
                     }
-                    myCss.insertRule(newStyle, myCss.cssRules.length);
                 }
-            
+                recNum=$("#game-recs section").length; 
         }
+
+        var pairsNum = Math.floor(recNum / 2); // number of pairs of letters
+        var lettersArr = []; // array to store the letter
+            // choose random letters and add them to the array
+            for (var i = 0; i < pairsNum; i++) {
+                var randomLetter = charArr[Math.floor(Math.random() * charArr.length)];
+                lettersArr.push(randomLetter);
+                lettersArr.push(randomLetter);
+            }    
+            // if there is an odd number of squares, add one random letter to the array
+            if (recNum % 2 !== 0) {
+                var randomLetter = charArr[Math.floor(Math.random() * charArr.length)];
+                lettersArr.push(randomLetter);
+            }
+            // shuffle the letters array
+            for (var i = lettersArr.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = lettersArr[i];
+                lettersArr[i] = lettersArr[j];
+                lettersArr[j] = temp;
+            }
+            //add letters to the recs
+            for(var i = 0; i<recNum;i++){
+                $('#game-rec'+i).text(lettersArr[i]);
+            }  
     });
 
+
     $('#game-recs').on('click', 'section', function() {
-        var clickedSection = $('#game-rec'+$(this).index());
-        clickedSection.css('color', 'white');
-        flag+=1;
-        if(flag==1){
-            clickedLetters.push(clickedSection.text());
-            clickedRecs.push(clickedSection.index());
+        var recNum=$("#game-recs section").length; 
+        if(recNum%2!==0){
+            $('.error-message').show();
         }
-        if(flag==2){
-            if(clickedSection.text()!=clickedLetters[0]){    
-                setTimeout(() => {
-                    clickedSection.css('color', 'black');
-                    $('#game-rec' + clickedRecs[0] ).css('color', 'black');
+        else{
+            var clickedSection = $('#game-rec'+$(this).index());
+            clickedSection.css('color', 'white');
+            flag+=1;
+            if(flag==1){
+                clickedLetters.push(clickedSection.text());
+                clickedRecs.push(clickedSection.index());
+            }
+            if(flag==2){
+                if(clickedSection.text()!=clickedLetters[0]){    
+                    setTimeout(() => {
+                        clickedSection.css('color', 'black');
+                        $('#game-rec' + clickedRecs[0] ).css('color', 'black');
+                        clickedLetters.splice(0);
+                        clickedRecs.splice(0);
+                        flag=0;
+                    }, 250);       
+                }
+                else{
                     clickedLetters.splice(0);
                     clickedRecs.splice(0);
-                    flag=0;
-                }, 250);       
-            }
-            else{
-                clickedLetters.splice(0);
-                clickedRecs.splice(0);
-                flag =0;
+                    flag =0;
+                }
             }
         }
     });
@@ -76,36 +109,34 @@ $(document).ready(function() {
     $(window).on('resize', function() {
         var screenWidth = $(window).width();
         var recNum=$("#game-recs section").length; 
+        if(recNum>1){
+            var lastSection = $('#game-recs section:last');
+            var lastSectionBottom = lastSection.position().top + lastSection.outerHeight();
+        }
+        if(prevScreenWidth < 850 && screenWidth >= 850 && recNum > 6 &&flag1) {
 
-        if(prevScreenWidth < 850 && screenWidth >= 850 && recNum == 9 &&flag1) {
-            $(".bottom-yellow-rec").css('top', '1200px');
-            $(".right-blue-rec").css('height', '1200px');
-            $(".left-grey-rec").css('height', '1200px');
+            $(".bottom-yellow-rec").css('top', lastSectionBottom+300);
+            $("#main3").css('height',lastSectionBottom+300);
             flag1 = 0;
             flag2 = 1;
-            console.log("sdfsd");
         }
 
-        if(prevScreenWidth < 850 && screenWidth >= 850 && recNum == 12 &&flag1) {
-            $(".bottom-yellow-rec").css('top', '2100px');
-            $(".right-blue-rec").css('height', '2100px');
-            $(".left-grey-rec").css('height', '2100px');
-            flag1 = 0;
-            flag2 = 1;
-            console.log("sdfsd");
-        }
-
-        if(prevScreenWidth > 850 && screenWidth <= 850&& recNum == 9 && flag2) {
-            $(".bottom-yellow-rec").css('top', '1700px');
+        if(prevScreenWidth > 850 && screenWidth <= 850&& recNum >6 && flag2) {
+            $(".bottom-yellow-rec").css('top', lastSectionBottom+300);
             flag1 = 1;
             flag2 = 0;
         }
 
-        if(prevScreenWidth > 850 && screenWidth <= 850&& recNum == 12 && flag2) {
-            $(".bottom-yellow-rec").css('top', '2500px');
-            flag1 = 1;
-            flag2 = 0;
-        }
         prevScreenWidth = screenWidth;
     });
+
+    $('.close-btn').on('click', function(){
+        $('.error-message').hide();
+    });
+
+    $('#topR-restart').on('click', function(){
+        location.reload();
+    });
 });
+
+
